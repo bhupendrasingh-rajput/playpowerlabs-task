55e3df5de323f1e25df895d8980b72ae1d930e79
Bhupendra Rajput <bjrajput15800@gmail.com>
2024-05-25 00:51:15 +0530

    Added my project

---
+++ time-converter/src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import TimezoneConverter from "./Components/Converter/TimezoneConverter";
+
+function App() {
+  return <div className="App">
+    <TimezoneConverter />
+  </div>
+}
+
+export default App
